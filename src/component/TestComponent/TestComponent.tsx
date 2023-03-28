fa9f1f22e4628f7d595624761df5fd72cd049a6a
tranvinhhung <tranvinhhung2k@gmail.com>
2023-03-28 18:50:23 +0700

    add code

--- src/component/TestComponent/TestComponent.tsx
+++ src/component/TestComponent/TestComponent.tsx
@@ -6,7 +6,18 @@ interface Props {
   data: any;
 }
 
-const Row = ({ index, style }: any) => <div style={style}>Row {index}</div>;
+const Row = ({ index, style }: any) => (
+  <div style={style}>
+    <Image
+      height={100}
+      width="100"
+      src={
+        "https://images.unsplash.com/photo-1679957907556-f79705b23c03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+      }
+    />{" "}
+    {index}
+  </div>
+);
 const TestComponent = (props: Props) => {
   const { dataSelector, count, handleSetCount } = useCustomHookTest();
   const [dataa, setData] = useState("vvvvvvv");
