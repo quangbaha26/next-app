b67467800b8ae183f21f8a574767d3dcd593d8f2
tranvinhhung <tranvinhhung2k@gmail.com>
2023-03-30 19:17:25 +0700

    test thu

--- pages/next-test/index.tsx
+++ pages/next-test/index.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import TestComponent from "../../src/component/TestComponent/TestComponent";
-const NextComponent = ({data}:any) => {
-  console.log("Dataaaaaaaaaa",data)
+const NextComponent = ({ data }: any) => {
+  console.log("Dataaaaaaaaaa", data);
   return <TestComponent data={data} />;
 };
 export async function getStaticProps() {
+  const mydata = () =>
+    new Promise((resolve, _) => {
+      setTimeout(() => {
+        resolve(1);
+      }, 3000);
+    });
+
+  const res = await mydata();
+
   return {
     props: {
-      data:'12313213'
+      data: res,
     }, // will be passed to the page component as props
-  }
+  };
 }
 
 export default NextComponent;
--- src/component/TestComponent/TestComponent.tsx
+++ src/component/TestComponent/TestComponent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import useCustomHookTest from "./handle/CustomHook/UseCustomHookTest";
@@ -6,27 +7,44 @@ interface Props {
   data: any;
 }
 
-const Row = ({ index, style }: any) => (
-  <div style={style}>
-    <Image
-      height={100}
-      width="100"
-      src={
-        "https://images.unsplash.com/photo-1679957907556-f79705b23c03?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
-      }
-    />{" "}
-    {index}
-  </div>
-);
 const TestComponent = (props: Props) => {
   const { dataSelector, count, handleSetCount } = useCustomHookTest();
   const [dataa, setData] = useState("vvvvvvv");
   console.log({ dataSelector, count });
 
+  const Row = ({ index, style }: any) => {
+    if (index % 2 === 0) {
+      return (
+        <div>
+          <img
+            height={100}
+            width="100"
+            src={
+              "https://images.unsplash.com/photo-1679936167452-18a6119f4006?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"
+            }
+          />
+          {index}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Image
+            src={
+              "https://images.unsplash.com/photo-1679952418656-d10950ac49e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+            }
+            width="100"
+            height={100}
+          />
+        </div>
+      );
+    }
+  };
+
   return (
     <div onClick={handleSetCount(count + 1)}>
-      helllo
-      <List height={1000} itemCount={1000} itemSize={35} width={"1000"}>
+      helllo {dataa}
+      <List height={1000} itemCount={100} itemSize={35} width={"1000"}>
         {Row}
       </List>
     </div>
